8f4ef0d9b1ba1c83143e9b877dcc480c32749b26
Michael Sharer <msharer@nhl.com>
2015-07-25 10:59:57 -0400

    Update for React Native v0.8.0
    The update includes additions to the Animations, Direct Manipulation and Navigator Comparisonto the Guides section and ActionSheetIOS to the Libraries section.
    
    Also updated npm dependencies for the build script.

--- src/indexedFiles.js
+++ src/indexedFiles.js
@@ -3,16 +3,23 @@ var indexedFiles = [
     { name: 'getting-started', type: 'Guides', toc: 'Section'},
     { name: 'troubleshooting', type: 'Guides', toc: 'Section'},
     { name: 'tutorial', type: 'Guides', toc: 'Section'},
+
     { name: 'style', type: 'Guides', toc: 'Section'},
     { name: 'gesture-responder-system', type: 'Guides', toc: 'Section'},
+    // new on 07/19/2015
+    { name: 'animations', type: 'Guides', toc: 'Section'},
     { name: 'nativemodulesios', type: 'Guides', toc: 'Section'},
     { name: 'nativecomponentsios', type: 'Guides', toc: 'Section'},
+    // new on 06/18/2015
+    { name: 'direct-manipulation', type: 'Guides', toc: 'Section'},
     { name: 'linking-libraries', type: 'Guides', toc: 'Section'},
     { name: 'debugging', type: 'Guides', toc: 'Section'},
     { name: 'testing', type: 'Guides', toc: 'Section'},
     { name: 'runningondevice', type: 'Guides', toc: 'Section'},
     { name: 'embedded-app', type: 'Guides', toc: 'Section'},
     { name: 'javascript-environment', type: 'Guides', toc: 'Section'},
+    // new on 06/17/2015
+    { name: 'navigator-comparison', type: 'Guides', toc: 'Section'},
     // videos.html is not included in the indexed files
 
     // Components
@@ -40,6 +47,8 @@ var indexedFiles = [
     { name: 'webview', type: 'Components', toc: 'Property'},
 
     //APIs - Library until APIs is available in Dash
+    // new on 06/15/2015
+    { name: 'actionsheetios', type: 'Library', toc: 'Property'},
     { name: 'alertios', type: 'Library', toc: 'Property'},
     { name: 'appregistry', type: 'Library', toc: 'Property'},
     { name: 'appstateios', type: 'Library', toc: 'Property'},
