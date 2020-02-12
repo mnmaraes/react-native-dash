a3320e04d41577a26315cdfea6005f406ee6afac
Murillo Nicacio <mnmaraes@gmail.com>
2020-02-12 08:18:28 +0000

    Updates for new react native website layout

--- src/createSectionJSON.js
+++ src/createSectionJSON.js
@@ -1,40 +1,58 @@
-var cheerio = require('cheerio');
-var fs = require('fs');
-var config = require('./config');
-var path = __dirname + '/../Contents/Resources/Documents/' + config.name + '/docs/getting-started.html';
-var indexedFilesPath = './src/indexedFiles.json';
-var src = fs.readFileSync(path, 'utf8');
-var $ = cheerio.load(src);
+var cheerio = require("cheerio");
+var fs = require("fs");
+var config = require("./config");
 
-var results = []
-$('div.navGroup').each(function (index, elem) {
-  var sectionHeader = $(elem).find('h3').text()
+var results = [];
 
-  $(elem).find('li').each(function(i, li) {
-    var page = {};
+var initialPages = ["getting-started.html", "activityindicator.html"];
 
-    page.name = $(li).find('a').attr('href').replace(/\.html$/, '').split('/').pop();
-    switch (sectionHeader) {
-        case 'Components':
-            page.type = 'Components';
-            page.toc = 'Property';
+initialPages.forEach(page => {
+  var path =
+    __dirname +
+    "/../Contents/Resources/Documents/" +
+    config.name +
+    `/docs/${page}`;
+  var src = fs.readFileSync(path, "utf8");
+
+  var $ = cheerio.load(src);
+  $("div.navGroup").each(function(index, elem) {
+    var sectionHeader = $(elem)
+      .find("h3")
+      .text();
+
+    $(elem)
+      .find("li")
+      .each(function(i, li) {
+        var page = {};
+
+        page.name = $(li)
+          .find("a")
+          .attr("href")
+          .replace(/\.html$/, "")
+          .split("/")
+          .pop();
+        switch (sectionHeader) {
+          case "Components":
+            page.type = "Components";
+            page.toc = "Property";
             break;
-        case 'APIs':
-            page.type = 'Library';
-            page.toc = 'Property';
+          case "APIs":
+            page.type = "Library";
+            page.toc = "Property";
             break;
-        case 'Polyfills':
-            page.type = 'Extension';
-            page.toc = 'Property';
+          case "Polyfills":
+            page.type = "Extension";
+            page.toc = "Property";
             break;
-        default:
+          default:
             page.type = config.defaultPageType;
             page.toc = config.defaultPageTOC;
-    };
+        }
 
-    results.push(page)
-  })
+        results.push(page);
+      });
+  });
 });
 
-fs.writeFileSync(indexedFilesPath, JSON.stringify(results) , 'utf-8');
-
+var indexedFilesPath = "./src/indexedFiles.json";
+fs.writeFileSync(indexedFilesPath, JSON.stringify(results), "utf-8");
--- src/modifyDocsHTML.js
+++ src/modifyDocsHTML.js
@@ -1,39 +1,58 @@
-var cheerio = require('cheerio');
-var fs = require('fs');
-var config = require('./config');
-var indexedFiles = require('./indexedFiles.json');
+var cheerio = require("cheerio");
+var fs = require("fs");
+var config = require("./config");
+var indexedFiles = require("./indexedFiles.json");
 
 // remove the left column and the nav bar so that it fits dash's usually small
 // browser screen
 indexedFiles.forEach(function(array, index) {
-    var path = __dirname + '/../Contents/Resources/Documents/' + config.name + '/docs/' + array.name + '.html';
-    var src = fs.readFileSync(path, 'utf8');
-    var $ = cheerio.load(src);
+  var path =
+    __dirname +
+    "/../Contents/Resources/Documents/" +
+    config.name +
+    "/docs/" +
+    array.name +
+    ".html";
+  var src = fs.readFileSync(path, "utf8");
+  var $ = cheerio.load(src);
 
-    var headerClasses = config.pageSubHeaders.toString();
-    var $headers = $(headerClasses);
+  var headerClasses = config.pageSubHeaders.toString();
+  var $headers = $(headerClasses);
 
-    $headers.each(function(index, elem) {
-        $('.edit-github').remove();
-        var name = $($(elem).contents().get(1)).text();
+  $headers.each(function(index, elem) {
+    $(".edit-github").remove();
+    var name = $(
+      $(elem)
+        .contents()
+        .get(1)
+    ).text();
 
-        // TODO: Change "array.toc to somehting more relevant on a page-by-page basis in indexedFiles.js"
-        $(elem).prepend('<a name="//apple_ref/cpp/' + array.toc + '/' + encodeURIComponent(name) + '" class="dashAnchor"></a>');
-        $.html();
-    });
+    // TODO: Change "array.toc to somehting more relevant on a page-by-page basis in indexedFiles.js"
+    $(elem).prepend(
+      '<a name="//apple_ref/cpp/' +
+        array.toc +
+        "/" +
+        encodeURIComponent(name) +
+        '" class="dashAnchor"></a>'
+    );
+    $.html();
+  });
 
-    // Update cdn url to display iframes.
-    $("iframe[data-src*='//cdn.rawgit.com']").each(function() {
-        $(this).attr('data-src', $(this).attr('data-src').replace('//cdn.rawgit.com','http://cdn.rawgit.com'));
-    });
+  // Update cdn url to display iframes.
+  $("iframe[data-src*='//cdn.rawgit.com']").each(function() {
+    $(this).attr(
+      "data-src",
+      $(this)
+        .attr("data-src")
+        .replace("//cdn.rawgit.com", "http://cdn.rawgit.com")
+    );
+  });
 
-    $('.nav-main').remove();
-    $('.nav-docs').remove();
-    $('.nav-footer, .edit-page-block').remove();
-    
-    $('.container').attr('style', 'min-width:inherit;padding-top:0');
-    $('.wrap').attr('style', 'width:inherit;');
-    $('.inner-content').attr('style', 'float:none;margin:auto;');
+  $(".fixedHeaderContainer").remove();
+  $(".navGroup").remove();
+  $(".docsNavContainer").remove();
 
-    fs.writeFileSync(path, $.html(), 'utf8');
+  $(".navPusher").attr("style", "padding-top:0");
+
+  fs.writeFileSync(path, $.html(), "utf8");
 });
